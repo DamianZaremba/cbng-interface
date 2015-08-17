819df732efc8fc523ad980c31b06c2fa14c39228
Damian Zaremba <damian@damianzaremba.co.uk>
2015-08-17 05:07:02 +0100

    Remove boilerplate

--- cbng_interface/auth.py
+++ cbng_interface/auth.py
@@ -3,7 +3,9 @@ from social.backends.oauth import BaseOAuth1
 
 class MediaWikiOAuth(BaseOAuth1):
     name = 'mediawiki'
-    AUTHORIZATION_URL = 'https://en.wikipedia.org/wiki/Special:OAuth/authorize'
-    REQUEST_TOKEN_URL = 'https://en.wikipedia.org/w/index.php?title=Special%3AOAuth%2Finitiate'
+    BASE_URL = 'https://en.wikipedia.org'
+    AUTHORIZATION_URL = '%s/wiki/Special:OAuth/authorize' % BASE_URL
+    REQUEST_TOKEN_URL = '%s/w/index.php?title=Special%3AOAuth%2Finitiate' % (
+        BASE_URL)
     REQUIRES_EMAIL_VALIDATION = False
     ACCESS_TOKEN_METHOD = 'POST'
--- cbng_interface/urls.py
+++ cbng_interface/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from django.conf import settings
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
