412e541eec0af2d21eb0614eea4cc69f5962d923
Damian Zaremba <damian@damianzaremba.co.uk>
2016-05-28 16:05:13 +0100

    Add explicit test

--- cbng_interface/utils.py
+++ cbng_interface/utils.py
@@ -12,19 +12,20 @@ def create_api_token(sender, user, request, **kwargs):
     except ApiKey.DoesNotExist:
         ApiKey.objects.create(user=user)
 
+
 def map_user_rights(sender, user, request, **kwargs):
     try:
         u = Users.objects.get(username=user.username)
     except Users.DoesNotExist:
         pass
     else:
-        if u.admin:
+        if u.admin == 1:
             try:
                 user.groups.add(Group.objects.get(name='reviewers'))
             except Group.DoesNotExist, e:
                 logger.error('Could not get reviwers group', e)
 
-        if u.superadmin:
+        if u.superadmin == 1:
             user.admin = True
             user.staff = True
 
